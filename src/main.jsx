508092d3b5b5652b00eb80d22aea74c739f0ba11
Angelpreet <angelpreetk276@gmail.com>
2026-03-09 15:31:07 +0530

    remove StrictMode fix double order

--- src/main.jsx
+++ src/main.jsx
@@ -9,6 +9,20 @@
 //   </StrictMode>,
 // )
 
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import App from "./App";
+// import "./index.css";
+// import { CartProvider } from "./context/CartContext";
+
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//   <React.StrictMode>
+//        <CartProvider>
+//     <App />
+//     </CartProvider>
+//   </React.StrictMode>
+// );
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
@@ -16,9 +30,7 @@ import "./index.css";
 import { CartProvider } from "./context/CartContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-       <CartProvider>
+  <CartProvider>
     <App />
-    </CartProvider>
-  </React.StrictMode>
+  </CartProvider>
 );
